118eace5c3776a629e89d58fe2cb64412c63216d
pgrp770 <pgrp770@gmail.com>
2024-12-16 09:11:11 +0200

    feat: init topics

--- kafka_settings/admin.py
+++ kafka_settings/admin.py
@@ -1,22 +1,39 @@
 import os
-from ensurepip import bootstrap
+import time
+
 from dotenv import load_dotenv
-from kafka_settings import KafkaAdminClient
-from kafka_settings.admin import NewTopic
-from kafka_settings.errors import TopicAlreadyExistsError
+from kafka import KafkaAdminClient
+from kafka.admin import NewTopic
+from kafka.errors import TopicAlreadyExistsError
 
 load_dotenv(verbose=True)
+list_topics = [
+    os.environ['NEO4J_TEACHERS_TOPIC'],
+    os.environ['NEO4J_CLASSES_TOPIC'],
+    os.environ['NEO4J_RELATIONS_TOPIC'],
+    os.environ['POSTGRES_STUDENT_PROFILE_TOPIC'],
+    os.environ['POSTGRES_STUDENT_LIFE_STILE_TOPIC'],
+    os.environ['POSTGRES_STUDENT_PERFORMANCE_TOPIC'],
+]
 
-def init_topics():
+def create_topic(topic_name):
     client = KafkaAdminClient(bootstrap_servers=os.environ['BOOTSTRAP_SERVERS'])
     new_member_topic = NewTopic(
-        name=os.environ['NEW_MEMBER_TOPIC'],
+        name=topic_name,
         num_partitions=int(os.environ['NUM_PARTITIONS']),
         replication_factor=int(os.environ['REPLICATION_FACTOR'])
     )
     try:
         client.create_topics([new_member_topic])
+        print(f"create topic {topic_name}")
     except TopicAlreadyExistsError as e:
         print(e)
     finally:
         client.close()
+def init_topics():
+    for topic in list_topics:
+        create_topic(topic)
+
+
+if __name__ == '__main__':
+    init_topics()
