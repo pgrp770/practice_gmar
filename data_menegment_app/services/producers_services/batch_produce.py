76ba721e3d658c7169d41f6f65a1a318a9e4436a
pgrp770 <pgrp770@gmail.com>
2024-12-17 10:28:07 +0200

    feat: add produce_cunks function to gericly produce chunks

--- data_menegment_app/services/producers_services/batch_produce.py
+++ data_menegment_app/services/producers_services/batch_produce.py
@@ -1,11 +1,5 @@
 import toolz as t
 from typing import List, Callable
 
-@t.curry
-def produce_with_chunks(chunks_size: int, data: List, produce: Callable):
-    chunk_data = list(t.partition_all(chunks_size, data))
-    print(chunk_data)
-
-
-
-
+def produce_chunks(data: List, produce: Callable, chunks_size: int):
+    [produce(item) for item in list(t.partition_all(chunks_size, data))]
